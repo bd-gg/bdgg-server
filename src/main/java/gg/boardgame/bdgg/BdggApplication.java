663f961d206e39c97c34f2accc53aec8a5bd6790
Taehyun Hwang <eeht1717@naver.com>
2020-05-06 05:42:23 +0900

    [feat] Add spring-data-jpa test example
    - spring-data-jpa 테스트
    - spring-data-jpa를 이용해 운영 DB 초기화 테스트

--- src/main/java/gg/boardgame/bdgg/BdggApplication.java
+++ src/main/java/gg/boardgame/bdgg/BdggApplication.java
@@ -1,7 +1,6 @@
 package gg.boardgame.bdgg;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/gg/boardgame/bdgg/account/AccountRepositoryTest.java
+++ src/test/java/gg/boardgame/bdgg/account/AccountRepositoryTest.java
@@ -11,6 +11,9 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -27,12 +30,18 @@ class AccountRepositoryTest {
 
     @Test
     public void di() throws SQLException {
-        try(Connection connection = dataSource.getConnection()) {
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getURL());
-            System.out.println(metaData.getDriverName());
-            System.out.println(metaData.getUserName());
-        }
-    }
+        Account account = new Account();
+        account.setUsername("Taehyun");
+        account.setPassword("pass");
 
+        Account newAccount = accountRepository.save(account);
+
+        assertThat(newAccount).isNotNull();
+
+        Optional<Account> existingAccount = accountRepository.findByUsername(newAccount.getUsername());
+        assertThat(existingAccount).isNotEmpty();
+
+        Optional<Account> nonExistingAccount = accountRepository.findByUsername("whiteship");
+        assertThat(nonExistingAccount).isEmpty();
+    }
 }

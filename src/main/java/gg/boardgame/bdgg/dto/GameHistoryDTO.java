e31a33607c7d7fa8494827bc0c55d307741ac1a8
Taehyun Hwang <eeht1717@gmail.com>
2020-05-13 02:43:22 +0900

    [feat] Add Lombok Annotation in DTO Class
    - Lombok Annotation 추가 (Data)
      - 기본 Getter, Setter Method 삭제

--- src/main/java/gg/boardgame/bdgg/dto/GameHistoryDTO.java
+++ src/main/java/gg/boardgame/bdgg/dto/GameHistoryDTO.java
@@ -1,22 +1,9 @@
 package gg.boardgame.bdgg.dto;
 
+import lombok.Data;
+
+@Data
 public class GameHistoryDTO {
     private int gameId;
     private int count;
-
-    public int getGameId() {
-        return gameId;
-    }
-
-    public void setGameId(int gameId) {
-        this.gameId = gameId;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
 }
--- src/main/java/gg/boardgame/bdgg/dto/IndividualGameResultDTO.java
+++ src/main/java/gg/boardgame/bdgg/dto/IndividualGameResultDTO.java
@@ -1,5 +1,8 @@
 package gg.boardgame.bdgg.dto;
 
+import lombok.Data;
+
+@Data
 public class IndividualGameResultDTO {
     private int userId;
     private String name;

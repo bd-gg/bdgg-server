e5371732e247ae2ea5847f63ca22575c99343add
JaewonKim <huhu420@naver.com>
2020-05-15 16:32:12 +0900

    [mod] Add lombok annotation to make getter setter function
    - DTO class에 @Data annotation 추가
    - Entity class @Getter @Setter annotation 추가

--- src/main/java/gg/boardgame/bdgg/dto/IndividualGameResultDTO.java
+++ src/main/java/gg/boardgame/bdgg/dto/IndividualGameResultDTO.java
@@ -7,28 +7,4 @@ public class IndividualGameResultDTO {
     private long userId;
     private String name;
     private int result;
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getResult() {
-        return result;
-    }
-
-    public void setResult(int result) {
-        this.result = result;
-    }
 }

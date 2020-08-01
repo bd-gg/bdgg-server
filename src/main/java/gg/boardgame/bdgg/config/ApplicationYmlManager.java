b71227e5262c125b39ab616e4170aa12ef90c6d8
Taehyun Hwang <eeht1717@gmail.com>
2020-08-02 00:37:47 +0900

    [feat] ApplicationYmlManager에 Getter/Setter 추가

--- src/main/java/gg/boardgame/bdgg/config/ApplicationYmlManager.java
+++ src/main/java/gg/boardgame/bdgg/config/ApplicationYmlManager.java
@@ -1,10 +1,14 @@
 package gg.boardgame.bdgg.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "jwt")
+@Setter
+@Getter
 public class ApplicationYmlManager {
     private String secret;
 

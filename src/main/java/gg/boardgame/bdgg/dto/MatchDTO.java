80169932efbf2c7af552748280a0fde809fd2685
Taehyun Hwang <eeht1717@gmail.com>
2020-08-31 04:16:50 +0900

    [feat] MatchDTO에 static nested class (Info, Request, Response) 추가

--- src/main/java/gg/boardgame/bdgg/dto/MatchDTO.java
+++ src/main/java/gg/boardgame/bdgg/dto/MatchDTO.java
@@ -2,10 +2,13 @@ package gg.boardgame.bdgg.dto;
 
 import gg.boardgame.bdgg.db.*;
 import gg.boardgame.bdgg.exception.ResourceNotFoundException;
-import lombok.Data;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,26 +17,55 @@ import java.util.Map;
 @Slf4j
 @Data
 public class MatchDTO {
-    private Long id;
-    private Long gameId;
-    private Integer gameType;
-    private Date playedTime;
-    private String place;
-    private String result;
-    private Long winnerId;
-    private List<Long> userIds = new ArrayList<>();
 
     public MatchDTO(Match match) {
-        this.id = match.getId();
-        this.gameId = match.getGameId();
-        this.gameType = match.getGameType();
-        this.playedTime = match.getPlayedTime();
-        this.place = match.getPlace();
-        this.result = match.getResult();
-        this.winnerId = match.getWinnerId();
-        /* get userIds */
-        match.getUserMatches().forEach(userMatch -> {
-            this.userIds.add(userMatch.getUser().getId());
-        });
     }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class Info {
+
+        public Info(Match match) {
+            this.id = match.getId();
+            this.gameId = match.getGameId();
+            this.gameType = match.getGameType();
+            this.playedTime = match.getPlayedTime();
+            this.place = match.getPlace();
+            this.result = match.getResult();
+            this.winnerId = match.getWinnerId();
+        }
+
+        private Long id;
+        private Long gameId;
+        private Integer gameType;
+        private Timestamp playedTime;
+        private String place;
+        private String result;
+        private Long winnerId;
+    }
+
+    @Getter
+    @Setter
+    public static class Request {
+        private Long gameId;
+        private Integer gameType;
+        private Timestamp playedTime;
+        private String place;
+        private String result;
+        private Long winnerId;
+        private List<Pair<Long,Long>> userScores = new ArrayList<>();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response {
+        private Info info;
+        private List<Long> userIds = new ArrayList<>();
+
+        public void setUserIds(List<Long> userIds) {
+            this.userIds = userIds;
+        }
+    }
+
 }
